618eea27126f531256b4e725401d87de7c93a604
Nathan Caldwell <saintdev@gmail.com>
2011-04-12 00:37:51 -0600

    Don't adjust the DC coefficient as suggested by Alex_W.
    I haven't tested this, but it seemed like a good idea.

--- common/quant.c
+++ common/quant.c
@@ -72,7 +72,7 @@ static int quant_8x8( dctcoef dct[64], udctcoef mf[64], udctcoef bias[64], int u
 
     for( int i = 0; i < 64; i++ ) {
         QUANT_ONE(quant[i], mf[i], bias[i]);
-        if( !quant[i] ) {
+        if( i && !quant[i] ) {
             en += CALC_EN( dct[i] );
             sort[count++] = dct + i;
         }
